572114780479d1ef1ced62fae5b03a4726c4174f
Yash Darji <yashdarji23082004@gmail.com>
2023-11-19 12:05:01 +0530

    trying  deployment

--- src/index.js
+++ src/index.js
@@ -32,8 +32,8 @@ client.on('ready', () => {
 client.initialize();
 
 //To Serve Qr code
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
 });
 
 client.on('message', async msg => {
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { serverQrCode } from './utils';
 
 export const app = express();
 
-const port = 3000
+
 
 app.get('/', (req, res) => {
   res.send('Hello, Server is running!!');
