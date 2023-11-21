d787e8a22d9e4a195014f8b464af1bcc8b7dff83
Yash Darji <yashdarji23082004@gmail.com>
2023-11-21 18:42:25 +0530

    Changed port

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ client.on('ready', () => {
 client.initialize();
 
 //To Serve Qr code
-app.listen(3000, () => {
+app.listen(3002, () => {
     console.log(`Server is running on port 3000`);
 });
 
