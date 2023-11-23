2f297a92e539eadba97ad11c0dbde980ca796d07
Yash Darji <yashdarji23082004@gmail.com>
2023-11-23 17:55:51 +0530

    Framework done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import pkg from 'whatsapp-web.js';
-const { Client, LocalAuth,MessageMedia } = pkg;
+const { Client, LocalAuth, MessageMedia, List, Buttons } = pkg;
 import qrcode from "qrcode";
 import Replicate from 'replicate';
 import dotenv from 'dotenv';
 import axios from 'axios';
 import fs from "fs"
 import { app } from './server';
+import { channel } from 'diagnostics_channel';
 
 dotenv.config();
 
@@ -13,11 +14,11 @@ dotenv.config();
 export const client = new Client({
     puppeteer: {
         headless: true,
-		args: [
+        args: [
             '--log-level=3', // fatal only
             '--start-maximized',
             '--no-default-browser-check',
-            '--disable-infobars', 
+            '--disable-infobars',
             '--disable-web-security',
             '--disable-site-isolation-trials',
             '--no-experiments',
@@ -35,8 +36,8 @@ export const client = new Client({
             '--no-first-run',
             '--no-zygote',
             '--single-process'
-              ],
-	},
+        ],
+    },
     authStrategy: new LocalAuth()
 });
 
@@ -50,7 +51,6 @@ client.on('ready', () => {
     console.log('Client is ready!');
 });
 
- 
 client.initialize();
 
 //To Serve Qr code
@@ -59,53 +59,83 @@ app.listen(3002, () => {
 });
 
 client.on('message', async msg => {
-	if(msg.body === '!ping') {
-		client.sendMessage(msg.from, 'pong');
-	}
-
-        if(msg.hasMedia) {
-            const image = await msg.downloadMedia();
-
-            if (image.mimetype.startsWith("image/")){
-                createImage(image,msg)
-                msg.reply("Image is Being Processed")
-            }
-
-        }
-
-     
+    messageResponse(msg);
 });
+
+let productName = '';
+let productImage = '';
+const messageResponse = async (msg) => {
+    const user = msg.author;
+    let chatId = msg.from;
+    const greetingMsg = `Basic Greeting Message`;
+    const sendImageMsg = `Please Send the Image`;
+    const askNameMsg = `Please write the name of the product`;
+    const processingMsg = `Please wait while image is being processed`
+
+    const chat = await msg.getChat()
+    const messages  = await chat.fetchMessages({fromMe:true});
+    
+    const lastMessage = messages[messages.length - 1].body
+    console.log(lastMessage)
+    
+
+    const downloadMedia = async (msg) =>{  
+        if (msg.type == 'image') {
+            return await msg.downloadMedia();
+        }
+    }
+
+    if (msg.body.toLowerCase().includes('service')) {
+        client.sendMessage(chatId,greetingMsg)
+        client.sendMessage(chatId,sendImageMsg)
+    } 
+    else if (lastMessage.includes("Image")) {
+        productImage = await downloadMedia(msg)
+        client.sendMessage(chatId,askNameMsg)
+    } 
+    else if(lastMessage.includes("name")){
+        productName = msg.body
+        console.log(productImage);
+        createImage(productImage,msg,productName);
+        client.sendMessage(chatId,processingMsg)
+    }
+
+    
+
+};
 
 const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN,
+    auth: process.env.REPLICATE_API_TOKEN,
 });
 
-const createImage = async (image,msg) => {
-    try{
+const createImage = async (image, msg, productName) => {
+    try {
         const base64Image = image.data.toString("base64");
         const mimeType = image.mimetype
         const dataURI = `data:${mimeType};base64,${base64Image}`;
-
+        // console.log(process.env.OPENAI_API)
         const output = await replicate.run(
-                "logerzhu/ad-inpaint:b1c17d148455c1fda435ababe9ab1e03bc0d917cc3cf4251916f22c45c83c7df",
-                {
-                    input: {
+            "logerzhu/ad-inpaint:b1c17d148455c1fda435ababe9ab1e03bc0d917cc3cf4251916f22c45c83c7df",
+            {
+                input: {
                     image_path: dataURI,
-                    prompt: "Product photography, outdoor setting, natural lighting, close-up shot, multiple angles, maintain aspect ratio, maintain height, maintain shadow",
-                    negative_prompt: "illustration, 3d, sepia, painting, cartoons, sketch, (worst quality:2),no distracting elements in the background"
-                    },
-                }
-                );
-        
-        for(const imageUrl of output){
-            const response = await axios.get(imageUrl, { responseType: 'arraybuffer'});
+                    prompt: `${productName || "Product" } photography, outdoor setting, natural lighting, close-up shot, multiple angles, maintain aspect ratio, maintain height, maintain shadow`,
+                    negative_prompt: "illustration, 3d, sepia, painting, cartoons, sketch, (worst quality:2),no distracting elements in the background",
+                    image_num: 1,
+                    // api_key: process.env.OPENAI_API
+                },
+            }
+        );
+
+        for (const imageUrl of output) {
+            const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
             const base64Image = Buffer.from(response.data, 'binary').toString('base64');
-            const image = new MessageMedia('image/png',base64Image)
-            msg.reply(image).then((res)=> console.log(res))
+            const image = new MessageMedia('image/png', base64Image)
+            msg.reply(image).then((res) => console.log(res))
         }
-    }   catch (error) {
+    } catch (error) {
         console.log(error);
         msg.reply("There has been a problem")
     }
-    
+
 }
