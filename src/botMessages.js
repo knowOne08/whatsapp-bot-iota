9ab28b57cbfc497b9d173744f4711c0ae7728b93
Yash Darji <yashdarji23082004@gmail.com>
2023-11-24 17:29:57 +0530

    correcting message style

--- src/botMessages.js
+++ src/botMessages.js
@@ -1,35 +1,35 @@
-export const creditsOverMsg = `
-🚫 *Snapcraft Free Credits Over*
+export const creditsOverMsg = 
+`🚫 *Snapcraft Free Credits Over* 🚫
 
 Uh-oh! It looks like your free credits for Snapcraft have run out. 😕 If you'd like to continue using our services, you can purchase additional credits. Feel free to reach out if you have any questions or need assistance. Thank you for using Snapcraft! 🌟
 `;
 
-export const greetingMsg = `
-🌟 *Welcome to Snapcraft Bot!* 🌟
+export const greetingMsg = 
+`🌟 *Welcome to Snapcraft Bot!* 🌟
 
 Hello there! I'm Snapcraft, your AI-enhanced image bot. 🤖✨
 `;
 
-export const sendImageMsg = `
-📷 *Send Image*
+export const sendImageMsg = 
+`📷 *Send Image*
 
 Ready to enhance your image! 🚀 Please send the image you'd like me to work on, and let the magic begin! ✨
 `;
 
-export const askNameMsg = `
-🤖 *Send Product Name*
+export const askNameMsg = 
+`🤖 *Send Product Name*
 
 Before we start, could you please provide the name of the product or describe what you're looking for? It will help personalize the enhancements. 🎨✨
 `;
 
-export const processingMsg = `
-⌛ *Processing Image*
+export const processingMsg = 
+`⌛ *Processing Image*
 
 Great choice! Sit tight while I work on enhancing your image. 🛠️✨ This might take a moment, but the results will be worth it! 🌈
 `;
 
-export const themeMsg = `
-🎨 **Choose Theme**
+export const themeMsg = 
+`🎨 **Choose Theme**
 
 Time to add some style! 🌟 Choose a theme for your enhanced image:
 
@@ -47,8 +47,8 @@ Time to add some style! 🌟 Choose a theme for your enhanced image:
 Please enter the number of your preferred option:
 `;
 
-export const endMsg = `
-🎉 *Congratulations!*
+export const endMsg = 
+`🎉 *Congratulations!*
 
 Your enhanced images are ready! 🖼️✨ Thank you for using Snapcraft Bot. If you have any more requests or need further assistance, feel free to ask. Enjoy your enhanced images! 🌟
 `;
